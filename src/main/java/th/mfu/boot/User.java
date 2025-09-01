f19bf820050b1c7fb6246a7ac39e317908b3ae72
Supitcha-01 <6731503043@lamduan.mfu.ac.th>
2025-09-01 09:51:48 +0000

    lab04

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -14,16 +14,28 @@ public User (String username , String firstname ,String lastname,String email){
         this.lastname=lastname;
         this.email=email;
 }
-public String getUsername(){
-    reeturn=username;
+public String getUsername() {
+    return username;
 }
-public String getFirstname(){
-    reeturn=firstname;
+public void setUsername(String username) {
+    this.username = username;
 }
-public String getLastname(){
-    reeturn=lastname;
+public String getFirstname() {
+    return firstname;
 }
-public String getEmail(){
-    reeturn=email;
+public void setFirstname(String firstname) {
+    this.firstname = firstname;
+}
+public String getLastname() {
+    return lastname;
+}
+public void setLastname(String lastname) {
+    this.lastname = lastname;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
 }
 }
