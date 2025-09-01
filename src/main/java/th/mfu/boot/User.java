fac88f0851301ce1fb1f912d808cab8863207b05
Supitcha-01 <6731503043@lamduan.mfu.ac.th>
2025-09-01 09:49:10 +0000

    lab04

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -2,5 +2,28 @@ package th.mfu.boot;
 
 public class User {
 
-    
+    private String username ;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+public User(){}
+public User (String username , String firstname ,String lastname,String email){
+        this.username=username;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.email=email;
+}
+public String getUsername(){
+    reeturn=username;
+}
+public String getFirstname(){
+    reeturn=firstname;
+}
+public String getLastname(){
+    reeturn=lastname;
+}
+public String getEmail(){
+    reeturn=email;
+}
 }
